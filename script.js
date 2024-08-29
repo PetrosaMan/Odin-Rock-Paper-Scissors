3d07ee432a75b548d230bdd244cb2f5345447946
PetrosaMan <dw.todd.eng@gmail.com>
2024-08-29 22:05:42 +0200

    Added switch code for userChoice to script.js and statement to head of html to prevent favicon error in console.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* Project: Rock Paper Scissors */
 
+'use strict'
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   switch (randomNumber) {
@@ -10,19 +12,27 @@ function getComputerChoice() {
     case 3:
       return "scissors";
     default:
-      return "invalid choice";
+      return "invalid randomNumber";
   }
 }
 
 function getHumanChoice() {
      let userChoice = +prompt('Enter choice(1-3)','');
-     return userChoice 
-
+     switch( userChoice ) {
+        case 1:
+          return 'rock';
+        case 2:
+          return 'paper';
+        case 3:
+          return 'scissors';
+        default:
+          return  'invalid choice'; 
+     }     
 }
 
 let computerChoice = getComputerChoice();
 console.log('Computer choice: ', computerChoice);
 
 let userChoice = getHumanChoice();
-console.log('User choice: ', userChoice);
+console.log('User choice: ', userChoice, typeof userChoice);
 
