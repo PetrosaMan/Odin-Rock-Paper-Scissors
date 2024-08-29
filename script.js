b3739d8ceb3acb237d7a5b1f6d5b5cc0338186cd
PetrosaMan <dw.todd.eng@gmail.com>
2024-08-29 17:01:55 +0200

    added getComputerChoice function code.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello from script file");
+/* Project: Rock Paper Scissors */
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  switch (randomNumber) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+    default:
+      return "invalid choice";
+  }
+}
+
+let choice = getComputerChoice();
+
+console.log(choice);
